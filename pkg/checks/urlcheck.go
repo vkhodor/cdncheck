1b73a7149e728c37fcaf131388acf0b2ff936f18
Viktor V. Khodorchenko <vkhodor@gmail.com>
2021-10-27 22:09:42 +0700

    urlcheck fix

--- pkg/checks/urlcheck.go
+++ pkg/checks/urlcheck.go
@@ -22,25 +22,38 @@ func (h *URLCheck) Check(host string) (bool, error) {
 
 	h.Logger.Debug("Retries: ", h.Retries)
 	h.Logger.Debug("Fails: ", h.Fails)
+	fails := 0
+	for i := 0; i < h.Retries; i++ {
+		if fails >= h.Fails {
+			return false, nil
+		}
+		h.Logger.Debug("Retry: ", i+1)
 
-	if h.Schema == "" {
-		h.Schema = "http"
+		if h.Schema == "" {
+			h.Schema = "http"
+		}
+		url := h.Schema + "://" + host + ":" + strconv.Itoa(h.Port) + "/" + h.Path
+		h.Logger.Debug("URLCheck: url = ", url)
+
+		client := http.Client{
+			Timeout: h.TimeoutSeconds * time.Second,
+		}
+
+		resp, err := client.Get(url)
+		if err != nil {
+			fails += 1
+		}
+
+		h.Logger.Debug("URLCheck: ", resp.StatusCode)
+
+		if !h.checkCode(resp.StatusCode) {
+			fails += 1
+		}
 	}
-	url := h.Schema + "://" + host + ":" + strconv.Itoa(h.Port) + "/" + h.Path
-	h.Logger.Debug("URLCheck: url = ", url)
-
-	client := http.Client{
-		Timeout: h.TimeoutSeconds * time.Second,
+	if fails >= h.Fails {
+		return false, nil
 	}
-
-	resp, err := client.Get(url)
-	if err != nil {
-		return false, err
-	}
-
-	h.Logger.Debug("URLCheck: ", resp.StatusCode)
-
-	return h.checkCode(resp.StatusCode), nil
+	return true, nil
 }
 
 func (h *URLCheck) checkCode(code int) bool {
